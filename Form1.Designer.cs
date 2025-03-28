facfc2f6cdc6bf715bd41a60d481851e7ea5b512
emiliano-nunez <emilianoabelnunez@gmail.com>
2025-03-28 12:25:40 -0300

    primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace DatosPersonales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblAdress = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblNoname = new System.Windows.Forms.Label();
+            this.lblNoLastName = new System.Windows.Forms.Label();
+            this.lblNoAge = new System.Windows.Forms.Label();
+            this.lblNoAdress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Black;
+            this.lblName.Location = new System.Drawing.Point(21, 26);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(81, 16);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "FIRST NAME";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.ForeColor = System.Drawing.Color.Black;
+            this.lblLastName.Location = new System.Drawing.Point(21, 54);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(76, 16);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "LAST NAME";
+            // 
+            // lblAge
+            // 
+            this.lblAge.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.ForeColor = System.Drawing.Color.Black;
+            this.lblAge.Location = new System.Drawing.Point(21, 80);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(31, 16);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "AGE";
+            // 
+            // lblAdress
+            // 
+            this.lblAdress.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblAdress.AutoSize = true;
+            this.lblAdress.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdress.ForeColor = System.Drawing.Color.Black;
+            this.lblAdress.Location = new System.Drawing.Point(20, 106);
+            this.lblAdress.Name = "lblAdress";
+            this.lblAdress.Size = new System.Drawing.Size(56, 16);
+            this.lblAdress.TabIndex = 3;
+            this.lblAdress.Text = "ADRESS";
+            // 
+            // lblResult
+            // 
+            this.lblResult.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.Black;
+            this.lblResult.Location = new System.Drawing.Point(21, 145);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(55, 16);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "RESULT";
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtName.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(125, 26);
+            this.txtName.MaxLength = 50;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(288, 23);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtLastName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLastName.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtLastName.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(125, 52);
+            this.txtLastName.MaxLength = 50;
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(288, 23);
+            this.txtLastName.TabIndex = 6;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtAge.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAge.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtAge.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(125, 78);
+            this.txtAge.MaxLength = 3;
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(67, 23);
+            this.txtAge.TabIndex = 7;
+            this.txtAge.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAge_KeyPress);
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtAdress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAdress.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtAdress.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdress.Location = new System.Drawing.Point(125, 104);
+            this.txtAdress.MaxLength = 50;
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(288, 23);
+            this.txtAdress.TabIndex = 8;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtResult.Enabled = false;
+            this.txtResult.Font = new System.Drawing.Font("Segoe Fluent Icons", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(24, 173);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(389, 152);
+            this.txtResult.TabIndex = 9;
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnAccept.FlatAppearance.BorderSize = 0;
+            this.btnAccept.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccept.Location = new System.Drawing.Point(108, 345);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(92, 31);
+            this.btnAccept.TabIndex = 10;
+            this.btnAccept.Text = "Accept";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(243, 345);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(92, 31);
+            this.btnCancel.TabIndex = 11;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblNoname
+            // 
+            this.lblNoname.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblNoname.AutoSize = true;
+            this.lblNoname.Font = new System.Drawing.Font("Segoe Fluent Icons", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNoname.ForeColor = System.Drawing.Color.Red;
+            this.lblNoname.Location = new System.Drawing.Point(106, 26);
+            this.lblNoname.Name = "lblNoname";
+            this.lblNoname.Size = new System.Drawing.Size(15, 19);
+            this.lblNoname.TabIndex = 12;
+            this.lblNoname.Text = "*";
+            this.lblNoname.Visible = false;
+            // 
+            // lblNoLastName
+            // 
+            this.lblNoLastName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblNoLastName.AutoSize = true;
+            this.lblNoLastName.Font = new System.Drawing.Font("Segoe Fluent Icons", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNoLastName.ForeColor = System.Drawing.Color.Red;
+            this.lblNoLastName.Location = new System.Drawing.Point(106, 54);
+            this.lblNoLastName.Name = "lblNoLastName";
+            this.lblNoLastName.Size = new System.Drawing.Size(15, 19);
+            this.lblNoLastName.TabIndex = 13;
+            this.lblNoLastName.Text = "*";
+            this.lblNoLastName.Visible = false;
+            // 
+            // lblNoAge
+            // 
+            this.lblNoAge.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblNoAge.AutoSize = true;
+            this.lblNoAge.Font = new System.Drawing.Font("Segoe Fluent Icons", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNoAge.ForeColor = System.Drawing.Color.Red;
+            this.lblNoAge.Location = new System.Drawing.Point(106, 80);
+            this.lblNoAge.Name = "lblNoAge";
+            this.lblNoAge.Size = new System.Drawing.Size(15, 19);
+            this.lblNoAge.TabIndex = 14;
+            this.lblNoAge.Text = "*";
+            this.lblNoAge.Visible = false;
+            // 
+            // lblNoAdress
+            // 
+            this.lblNoAdress.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblNoAdress.AutoSize = true;
+            this.lblNoAdress.Font = new System.Drawing.Font("Segoe Fluent Icons", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNoAdress.ForeColor = System.Drawing.Color.Red;
+            this.lblNoAdress.Location = new System.Drawing.Point(105, 106);
+            this.lblNoAdress.Name = "lblNoAdress";
+            this.lblNoAdress.Size = new System.Drawing.Size(15, 19);
+            this.lblNoAdress.TabIndex = 15;
+            this.lblNoAdress.Text = "*";
+            this.lblNoAdress.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(435, 393);
+            this.Controls.Add(this.lblNoAdress);
+            this.Controls.Add(this.lblNoAge);
+            this.Controls.Add(this.lblNoLastName);
+            this.Controls.Add(this.lblNoname);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtAdress);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblAdress);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(451, 432);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PERSONAL DATA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblAdress;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblNoname;
+        private System.Windows.Forms.Label lblNoLastName;
+        private System.Windows.Forms.Label lblNoAge;
+        private System.Windows.Forms.Label lblNoAdress;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.SymbolStore;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace DatosPersonales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "" && txtLastName.Text != "" && txtAge.Text != "" && txtAdress.Text != "")
+            {
+                int age = int.Parse(txtAge.Text);
+                if (age > 0 && age < 140) 
+                { 
+                    txtResult.Text = 
+                                "Last Name & First Name: " + txtLastName.Text + " " + txtName.Text + "\r\n" + 
+                                "Age: " + age + "\r\n" + 
+                                "Adress: " + txtAdress.Text;
+
+                    txtLastName.BackColor = Color.White;
+                    txtAge.BackColor = Color.White;
+                    txtAdress.BackColor = Color.White;
+                }
+                else
+                {
+                    txtAge.BackColor = Color.Red;
+                    txtResult.Text = "Incorrect age";
+                }
+            }
+
+            ValidateTextBox(txtName, lblNoname);
+            ValidateTextBox(txtLastName, lblNoLastName);
+            ValidateTextBox(txtAge, lblNoAge);
+            ValidateTextBox(txtAdress, lblNoAdress);
+
+        }
+
+        // función para validar los campos de texto
+        private void ValidateTextBox(TextBox textBox, Label label)
+        {
+            label.Visible = string.IsNullOrEmpty(textBox.Text) ? label.Visible = true : label.Visible = false;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if ((e.KeyChar < 48 || e.KeyChar > 59) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+    }
+}
